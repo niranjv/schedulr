bb10308d3641b833e8755e9951bda97ecefbdeb2
Niranjan <niranjanv+github@gmail.com>
2015-02-25 02:50:17 -0800

    Closes #2 - add package docs

---
+++ R/schedulr.R
@@ -0,0 +1,11 @@
+#' schedulr: schedule jobs on machines
+#'
+#' schedulr provides functions to schedule jobs on machines based on type of job, type of machine and various input constraints
+#'
+#' To learn more about schedulr, start with the vignettes:
+#' \code{browseVignettes(package = "schedulr")}
+#'
+#' @docType package
+#' @name schedulr
+#' @author Niranjan Vissa (niranjanv+schedulr@@gmail.com)
+NULL
