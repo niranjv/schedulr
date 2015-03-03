307f0ed8e34e3983e14072023d8e987e5ad648a5
Niranjan <niranjanv+github@gmail.com>
2015-03-02 20:22:36 -0800

    #13 - update tests for get.temperature()

--- tests/testthat/test-get-temperature.R
+++ tests/testthat/test-get-temperature.R
@@ -5,7 +5,9 @@ test_that("get.temperature validates max.temp correctly", {
 
   expect_error(get.temperature(), 'Missing required argument')
   expect_error(get.temperature(c()), 'Invalid argument length')
+  expect_error(get.temperature('a'), 'Non-numeric argument')
   expect_error(get.temperature(1:2), 'Invalid argument length')
+  expect_error(get.temperature(-1), 'Invalid argument')
   expect_error(get.temperature(0), 'Invalid argument')
 
 })
@@ -18,6 +20,7 @@ test_that("get.temperature validates max.iter correctly", {
   expect_error(get.temperature(25, 'a'), 'Non-integer argument')
   expect_error(get.temperature(25, '3.14'), 'Non-integer argument')
   expect_error(get.temperature(25, 1:2), 'Invalid argument length')
+  expect_error(get.temperature(25, -1), 'Invalid argument')
   expect_error(get.temperature(25, 0), 'Invalid argument')
 
 })
@@ -30,6 +33,7 @@ test_that("get.temperature validates cur.iter correctly", {
   expect_error(get.temperature(25, 100, 'a'), 'Non-integer argument')
   expect_error(get.temperature(25, 100, '3.14'), 'Non-integer argument')
   expect_error(get.temperature(25, 100, 1:2), 'Invalid argument length')
+  expect_error(get.temperature(25, -1), 'Invalid argument')
   expect_error(get.temperature(25, 100, 101), 'Invalid argument')
 
 })
