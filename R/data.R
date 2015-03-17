2ce50eb90f32aa19802d36263a9ad683d7a4c8ff
Niranjan <niranjanv+github@gmail.com>
2015-03-16 21:40:44 -0700

    #5 - update docs for dataset

--- R/data.R
+++ R/data.R
@@ -1,6 +1,8 @@
-#' Runtimes on m3xlarge instance
-#'
-#' Dataset containing exponentially distributed runtimes generated based on mean runtimes for jobs run on m3xlarge instances in EC2
+#' Runtimes for tasks on m3xlarge instances
+#' '
+#' Dataset containing runtimes for tasks of various sizes. The runtimes for each
+#' task size are generated from an exponential distribution. The mean of the 
+#' exponential distribution is proportional to the task size.
 #'
 #' @format A data frame with 21750 rows and 2 variables
 #' \describe{
